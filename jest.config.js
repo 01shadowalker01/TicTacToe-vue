ef2008eeb81f84f5783134723c931298f13208a4
shadowalker <saeed.bayat@arusha.ir>
2021-11-05 00:44:34 +0330

    Change configs to run tests correctly

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,9 @@ module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
   transform: {
     '^.+\\.vue$': 'vue-jest'
-  }
+  },
+  testMatch: [
+    '<rootDir>/src/**/*.test.js',
+    '<rootDir>/src/**/*.test.ts',
+  ],
 }
